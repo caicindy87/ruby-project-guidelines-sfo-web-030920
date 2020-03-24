51e91160cf242cf4c8430dc43eda26be1406fe9e
caicindy87 <caicindy87@gmail.com>
2020-03-23 17:44:25 -0700

    Created new migration to create users table

---
+++ lib/api_communicator.rb
@@ -0,0 +1,7 @@
+# require 'rest-client'
+# require 'json'
+
+# def get_businesses_from_yelp_api
+#     response_string = RestClient.get('https://api.yelp.com/v3/businesses/search')
+#     response_hash = JSON.parse(response_string)
+# end
