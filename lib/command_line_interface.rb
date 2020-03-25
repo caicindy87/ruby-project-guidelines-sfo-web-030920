cb1dcf72805db062381aeb54b05f4b28562c7198
caicindy87 <caicindy87@gmail.com>
2020-03-25 11:11:25 -0700

    Added method to create new Place record depending on which location user wants to save

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -40,34 +40,56 @@ end
 
 
 def start_search
-    puts 'enter 1 to find 5 random places'
+    puts 'Enter go to find five random places.'
     user_input = gets.chomp # user input is string data type
-    if user_input.to_i == 1 # can change user input to integer
+    if user_input ==  "go"                                  # can change user input to integer using .to_i
         display_five_place_names
     else
-        puts "bye"
+        # puts "I do not recognize that command. I said enter 'go.' "
+        puts "I said enter 'go.' I'm sorry I thought you could read... "
+        # if the response is not what we want, how do we prevent next methods from running? - need to add custom error
     end
 end
 
-# if the response is not what we want, how do we prevent next methods from running? - need to add custom error
+
 def want_to_save?
-    puts "press 1 to save option 1"
-    puts "press 2 to save option 2"
-    puts "press 3 to save option 3"
-    puts "press 4 to save option 4"
-    puts "press 5 to save option 5"
-    puts "press 0 to see next five results"
+    puts "enter 'one' to save option 1"
+    puts "enter 'two' to save option 2"
+    puts "enter 'three' to save option 3"
+    puts "enter 'four' to save option 4"
+    puts "enter 'five' to save option 5"
+    puts "enter 'next' to see next five results"
 end
 
 def get_response_to_save_or_not
-    response = gets.chomp.to_i
+    response = gets.chomp
 end
+
 
 # create 1 new Place instance / object
 # get_businesses_from_yelp_api returns an array of hashes
-def create_place_obj
-    option_one = get_businesses_from_yelp_api[0]
-    Place.create(name: option_one["name"], address: "#{option_one["location"]["display_address"][0]}, #{option_one["location"]["display_address"][1]}")
+def create_place_record(response)
+    if response == 'one'
+        option_one = get_businesses_from_yelp_api[0]
+        Place.create(name: option_one["name"], address: "#{option_one["location"]["display_address"][0]}, #{option_one["location"]["display_address"][1]}")
+    elsif response == 'two'
+        option_two = get_businesses_from_yelp_api[1]
+        Place.create(name: option_two["name"], address: "#{option_two["location"]["display_address"][0]}, #{option_two["location"]["display_address"][1]}")
+    elsif response == 'three'
+        option_three = get_businesses_from_yelp_api[2]
+        Place.create(name: option_three["name"], address: "#{option_three["location"]["display_address"][0]}, #{option_three["location"]["display_address"][1]}")
+    elsif response == 'four'
+        option_four = get_businesses_from_yelp_api[3]
+        Place.create(name: option_four["name"], address: "#{option_four["location"]["display_address"][0]}, #{option_four["location"]["display_address"][1]}")
+    elsif response == 'five'
+        option_five = get_businesses_from_yelp_api[4]
+        Place.create(name: option_five["name"], address: "#{option_five["location"]["display_address"][0]}, #{option_five["location"]["display_address"][1]}")
+    end
+end
+
+def create_saved_location_record
+    
+    SavedLocation(user: , place: , name:)
 end
 
 
